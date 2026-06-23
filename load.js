5fbf154ba8f7782c688876021179529ca782e342
jek-archives <quejada.jakelloyd@gmail.com>
2026-06-24 06:59:45 +0800

    Change load.js to load standalone.html via raw.githack.com

--- load.js
+++ load.js
@@ -1,2 +1,2 @@
 h5gg.require(7.9);
-h5gg.loadWebView("https://cdn.jsdelivr.net/gh/jek-archives/nba2k26editor@main/standalone.html");
+h5gg.loadWebView("https://raw.githack.com/jek-archives/nba2k26editor/main/standalone.html");
